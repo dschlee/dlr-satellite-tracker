753e950e6ff6a5f7bca175b94f53193c7075bbfd
Dennis Schlee <dennis.schlee@outlook.com>
2024-06-18 21:53:55 +0200

    Implement contrail of satellite

--- src/satellite_tracker.py
+++ src/satellite_tracker.py
@@ -1,53 +1,54 @@
-# importing libraries 
-# import the necessary packages
 from collections import deque
-from imutils.video import VideoStream
 import numpy as np
-import argparse
 import cv2
 import imutils
 import time
-  
-# Create a VideoCapture object and read from input file 
-cap = cv2.VideoCapture("./data/DLR_Satellite_Tracking_1.mp4") 
+
+# Create a VideoCapture object and read from input file
+cap = cv2.VideoCapture("./data/DLR_Satellite_Tracking_1.mp4")
 
 lower_threshold = 160
 upper_threshold = 255
-# pts = deque(maxlen=args["buffer"])
+buffer = 64
+pts = deque(maxlen=buffer)
 
-# allow the camera or video file to warm up
-time.sleep(1.0)
-  
-# Check if camera opened successfully 
-if (cap.isOpened()== False): 
-    print("Error opening video file") 
-  
-# Read until video is completed 
-while(cap.isOpened()): 
-      
-    # Capture frame-by-frame 
-    ret, frame = cap.read() 
-    
+# # allow the video file to warm up
+# time.sleep(1.0)
+
+# Check if video opened successfully
+if cap.isOpened() == False:
+    print("Error opening video file")
+
+# Read until video is completed
+while cap.isOpened():
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # if we are viewing a video and we did not grab a frame,
+    # then we have reached the end of the video
+    if frame is None:
+        break
+
     # resize the frame, blur it, and convert it to the Grayscale
     # color space
     frame = imutils.resize(frame, width=1000)
     # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+
     # construct a mask for the color "green", then perform
     # a series of dilations and erosions to remove any small
     # blobs left in the mask
     mask = cv2.inRange(gray, lower_threshold, upper_threshold)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
-    
-    
+
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     center = None
-    
+
     # only proceed if at least one contour was found
     if len(cnts) > 0:
         # find the largest contour in the mask, then use
@@ -61,29 +62,43 @@ while(cap.isOpened()):
         if radius > 1:
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(frame, center, 1, (0, 0, 255), -1)
+            cv2.circle(
+                frame,
+                center=(int(x), int(y)),
+                radius=int(radius),
+                color=(0, 255, 255),
+                thickness=2,
+            )
+            cv2.circle(frame, center=center, radius=1, color=(0, 0, 255), thickness=-1)
+
     # update the points queue
-    # pts.appendleft(center)
-    
-    
-    
-    
-    if ret == True: 
-    # Display the resulting frame 
-        cv2.imshow("Frame", frame) 
-          
-        # Press Q on keyboard to exit 
-        if cv2.waitKey(25) & 0xFF == ord('q'): 
-            break
-  
-    # Break the loop 
-    else: 
+    pts.appendleft(center)
+
+    # loop over the set of tracked points
+    for i in range(1, len(pts)):
+        # if either of the tracked points are None, ignore
+        # them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+        # otherwise, compute the thickness of the line and
+        # draw the connecting lines
+        thickness = int(np.sqrt(buffer / float(i + 1)) * 1)
+        cv2.line(
+            frame, pt1=pts[i - 1], pt2=pts[i], color=(0, 0, 255), thickness=thickness
+        )
+
+    # show the frame to our screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
         break
-  
-# When everything done, release 
-# the video capture object 
-cap.release() 
-  
-# Closes all the frames 
-cv2.destroyAllWindows() 
+
+
+# When everything done, release
+# the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
